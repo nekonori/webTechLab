1b1392e59202f48092ddfb8521feb3fcaccff378
nekonori <102738752+nekonori@users.noreply.github.com>
2022-09-21 11:04:08 +0530

    update regex

--- 06/JobSeeker.java
+++ 06/JobSeeker.java
@@ -5,7 +5,7 @@ public class JobSeeker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Username: ");
-        if (!Pattern.matches("[a-zA-Z0-9]{3,}_job", sc.nextLine()))
+        if (!Pattern.matches("^[a-zA-Z0-9]{3,}_job$", sc.nextLine()))
             System.out.println("Invalid username");
         System.out.print("Email: ");
         Pattern emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
@@ -13,7 +13,7 @@ public class JobSeeker {
         if (!emailPattern.matcher(sc.nextLine()).find())
             System.out.println("Invalid email");
         System.out.print("Password: ");
-        if (!Pattern.matches("[a-zA-Z0-9]*", sc.nextLine()))
+        if (!Pattern.matches("^[a-zA-Z0-9]*$", sc.nextLine()))
             System.out.println("Invalid password");
         sc.close();
     }
