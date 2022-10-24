58c000e80bab3aa29f2e88e53f870a8107e8b4ba
Pragadesh BS <pragadeshbs@protonmail.com>
2022-10-24 13:04:04 +0530

    mod exp9

--- 09/q1Client.java
+++ 09/q1Client.java
@@ -1,30 +1,25 @@
-import java.util.*;
 import java.io.*;
 import java.net.*;
 
-public class q1Client{
-    public static void main(String[] args)
-    {
-        try{
-            Socket s=new Socket("localhost",4000);
-            DataInputStream dis=new DataInputStream(s.getInputStream());
-            DataOutputStream dos=new DataOutputStream(s.getOutputStream());
-            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-            String ipStr="",opStr="";
-            while(!ipStr.equals("stop"))
-            {
+public class q1Client {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("localhost", 4000);
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String ipStr = "", opStr = "";
+            while (!ipStr.equals("stop")) {
                 System.out.print(">>>");
-                opStr=br.readLine();
+                opStr = br.readLine();
                 dos.writeUTF(opStr);
                 dos.flush();
-                ipStr=(String)dis.readUTF();
-                System.out.println("Message from server-->"+ipStr);
+                ipStr = (String) dis.readUTF();
+                System.out.println("Message from server-->" + ipStr);
             }
             dos.close();
             s.close();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
--- 09/q2Client.java
+++ 09/q2Client.java
@@ -1,32 +1,27 @@
-import java.io.*;
 import java.net.*;
 import java.util.*;
-class q2Client{
-    public static void main(String[] args){
-        try{
-            DatagramSocket dsclient=new DatagramSocket();
-            InetAddress ipaddr=InetAddress.getByName("localhost");
-            Scanner in=new Scanner(System.in);
-            //BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-            byte[] senddata=new byte[1024];
-            byte[] recdata=new byte[1024];
-            while(true){
-                System.out.print("Msg to server-->");
-                String tosend=in.nextLine();
-                //String tosend=br.readLine();
-                if(tosend.equalsIgnoreCase("bye")) break;
-                senddata=tosend.getBytes();
-                DatagramPacket sendpacket=new DatagramPacket(senddata,senddata.length,ipaddr,5000);
-                dsclient.send(sendpacket);
-                DatagramPacket recpacket=new DatagramPacket(recdata,recdata.length);
-                dsclient.receive(recpacket);
-                String rec_frm_ser=new String(recpacket.getData());
-                System.out.println("Msg from server-->"+rec_frm_ser.toLowerCase());
-            }
-            dsclient.close();
-            }
-        catch(Exception x){
-            x.printStackTrace();
+
+class q2Client {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket dsclient = new DatagramSocket();
+        InetAddress ipaddr = InetAddress.getByName("localhost");
+        Scanner in = new Scanner(System.in);
+        byte[] senddata = new byte[1024];
+        byte[] recdata = new byte[1024];
+        while (true) {
+            System.out.print("Msg to server-->");
+            String tosend = in.nextLine();
+            if (tosend.equalsIgnoreCase("bye"))
+                break;
+            senddata = tosend.getBytes();
+            DatagramPacket sendpacket = new DatagramPacket(senddata, senddata.length, ipaddr, 5000);
+            dsclient.send(sendpacket);
+            DatagramPacket recpacket = new DatagramPacket(recdata, recdata.length);
+            dsclient.receive(recpacket);
+            String rec_frm_ser = new String(recpacket.getData());
+            System.out.println("Msg from server-->" + rec_frm_ser.toLowerCase());
         }
+        dsclient.close();
+        in.close();
     }
 }
